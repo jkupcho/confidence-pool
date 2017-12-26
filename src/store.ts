d7e88bd4b75a78d7230a890df1155ae38b65253b
Jonathan Kupcho <jonathan.kupcho@gmail.com>
2017-12-26 15:58:54 -0600

    added initialState

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,32 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-const initialState = {};
+const initialState = {
+  games: [
+    { name: 'Pinstripe', teams: [ 'Iowa', 'Boston College' ] },
+    { name: 'Foster Farms', teams: [ 'Arizona', 'Purdue' ] },
+    { name: 'Military', teams: [ 'Virginia', 'Navy' ] },
+    { name: 'Camping', teams: [ 'Virginia Tech', 'Oklahoma State' ] },
+    { name: 'Alamo', teams: [ 'Stanford', 'TCU' ] },
+    { name: 'Holiday', teams: [ 'Washington State', 'Michigan State' ] },
+    { name: 'Belk', teams: [ 'Wake Forest', 'Texas A&M' ] },
+    { name: 'Sun', teams: [ 'NC State', 'Arizona State' ] },
+    { name: 'Music City', teams: [ 'Kentucky', 'Northwestern' ] },
+    { name: 'Arizona', teams: [ 'Utah State', 'New Mexico State' ] },
+    { name: 'Cotton', teams: [ 'USC', 'Ohio State' ] },
+    { name: 'TaxSlayer', teams: [ 'Louisville', 'Mississippi State' ] },
+    { name: 'Liberty', teams: [ 'Iowa State', 'Memphis' ] },
+    { name: 'Fiesta', teams: [ 'Washington', 'Penn State' ] },
+    { name: 'Orange', teams: [ 'Miami', 'Wisconsin' ] },
+    { name: 'Outback', teams: [ 'Michigan', 'South Carolina' ] },
+    { name: 'Peach', teams: [ 'UCF', 'Auburn' ] },
+    { name: 'Citrus', teams: [ 'Notre Dame', 'LSU' ] },
+    { name: 'Rose', teams: [ 'Oklahoma', 'Georgia' ] },
+    { name: 'Sugar', teams: [ 'Alabama', 'Clemson' ] },
+    { name: 'Championship', teams: [] },
+  ],
+  picks: [],
+};
 
 const store = createStore(
   rootReducer,
