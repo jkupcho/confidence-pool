6d11407f7564a022bed8c2de4a1cab8bbd108382
jkupcho <jonathan.kupcho@gmail.com>
2018-01-02 13:54:40 -0600

    started storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+
+  // For example, add typescript loader:
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    include: path.resolve(__dirname, '../src'),
+    loader: require.resolve('ts-loader')
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  return config;
+};
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import * as React from 'react';
+
+import Game from '../src/components/Game';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+// import { action } from '@storybook/addon-actions';
+// import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+// import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('Game', module)
+  .add('no winner', () => <Game />);
+
+// storiesOf('Button', module)
+//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
